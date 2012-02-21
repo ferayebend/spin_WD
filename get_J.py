a5d06cba87519696c9a1bfc88cb44863f552eb86
ferayebend <kulebi@ice.cat>
2012-02-21 18:03:24 +0100

    data.in inputunu kullanan script yazildi

--- get_J.py
+++ get_J.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 from pylab import *
-from scipy import interpolate
+from os import popen
 
 def loadData(inputFile):
     data = []
@@ -13,9 +13,10 @@ def loadData(inputFile):
 def transpose(data):
         return [[data[j][i] for j in range(len(data))] for i in range(len(data[0]))]
 
-if __name__ == '__main__':
-    data = transpose(loadData(open('datums.txt')))
-    mass_star = 1.1
+def getSphData():
+    data = transpose(loadData(open('./datums.txt')))
+    mass_star = 1.1	#need to input these
+    mass_disk = 0.3	#need to input these
     J_star = 0.
     J_disk = 0.
     J_total = 0.
@@ -25,6 +26,29 @@ if __name__ == '__main__':
 	   J_star = J_star + data[2][i]
 	else:
 	   J_disk = J_disk + data[2][i]
-    print J_star, J_disk, 'total: ', J_star + J_disk, J_total
-    plot(data[1],data[2],'ro')
-    show()
+    #print J_star, J_disk, 'total: ', J_star + J_disk, J_total
+    #plot(data[1],data[2],'ro')
+    #show()
+    return J_star, J_disk, mass_star, mass_disk
+
+if __name__ == '__main__':
+   J_star, J_disk, M_star, M_disk = getSphData()
+   print '''
+		*** Reading the out of the SPH data file *** 
+
+	 '''
+   Blist = [2e6,2e7,1e8,2e8,6e8]
+   print '''
+		*** writing the angular momentum data into data.in 
+		*** and
+		*** running spin_WD for B values of
+	 ''', Blist, ' G'
+   for B in Blist:
+	spinfile = open('data.in','w')
+   	line = '%1.3e %3.2e %3.4e %3.2e %3.4e'%(B,M_star,J_star,M_disk,J_disk)
+   	spinfile.write(line)
+   	spinfile.close()
+   	popen('./spin_WD')
+   	popen('mv star.out star_%1.1e.out'%B)
+   	popen('mv disk.out disk_%1.1e.out'%B)
+   print ''' finished '''
